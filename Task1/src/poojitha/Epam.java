5359811b5579f021518ac70cb94bcc16ad5b7b2a
PoojithaGangidi <poojithareddy.9051@gmail.com>
2020-07-26 17:01:20 +0530

    commit

---
+++ Task1/src/poojitha/Epam.java
@@ -0,0 +1,14 @@
+package poojitha;
+
+public class Epam {
+	void method()
+	{
+		System.out.println("Welcome");
+	}
+public static void main(String[] args)
+{
+	Epam e=new Epam();
+	System.out.println("Hello");
+	e.method();
+}
+}
